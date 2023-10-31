86698efb8450fe6e242ef780971bf21417485ec4
woohaen88 <woohaen88@gmail.com>
2023-11-01 02:59:30 +0900

    Feat: camping/ user serializer 추가

--- serializers/camping.go
+++ serializers/camping.go
@@ -1,26 +1,49 @@
 package serializers
 
 import (
+	"camping-backend/enums"
 	"camping-backend/models"
 	"time"
 )
 
 type Camping struct {
-	ID        uint   `json:"id"`
-	Title     string `json:"title"`
-	Address   string `json:"address"`
+	ID             uint
+	Title          string
+	Address        string
+	Description    string
+	View           enums.ViewKind
+	IsEvCharge     enums.Status
+	MannerTime     string // TODO: parseDateTIme
+	IsSideParking  enums.Status
+	IsPetFriendly  enums.Status
+	VisitedStartAt string // TODO: parseDateTIme
+	VisitedEndAt   string // TODO: parseDateTIme
+
+	UserID uint
+	User   User // Serializer
+
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	User      User
 }
 
-func CampingSerializer(camping models.Camping, user User) Camping {
+func CampingSerializer(camping *models.Camping, serializeUser User) Camping {
 	return Camping{
-		ID:        camping.ID,
-		Title:     camping.Title,
-		Address:   camping.Address,
+		ID:             camping.ID,
+		Title:          camping.Title,
+		Address:        camping.Address,
+		Description:    camping.Description,
+		View:           camping.View,
+		IsEvCharge:     camping.IsEvCharge,
+		MannerTime:     camping.MannerTime,
+		IsSideParking:  camping.IsSideParking,
+		IsPetFriendly:  camping.IsPetFriendly,
+		VisitedStartAt: camping.VisitedStartAt,
+		VisitedEndAt:   camping.VisitedEndAt,
+
+		UserID: serializeUser.ID,
+		User:   serializeUser,
+
 		CreatedAt: camping.CreatedAt,
 		UpdatedAt: camping.UpdatedAt,
-		User:      user,
 	}
 }
--- serializers/user.go
+++ serializers/user.go
@@ -2,17 +2,16 @@ package serializers
 
 import "camping-backend/models"
 
-
 type User struct {
-	ID uint `json:"id"`
-	Email string `json:"email"`
+	ID       uint   `json:"id"`
+	Email    string `json:"email"`
 	Username string `json:"username"`
 }
 
-func UserSerializer(userModel models.User)  User {
+func UserSerializer(userModel *models.User) User {
 	return User{
-		ID: userModel.ID,
-		Email: userModel.Email,
+		ID:       userModel.ID,
+		Email:    userModel.Email,
 		Username: userModel.Username,
 	}
 }
